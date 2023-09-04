2be2fa600fd78df9858a3e8640cb1101c8e32e56
Nudlik <gegorp@mail.com>
2023-09-05 01:47:39 +0700

    realized class PLVideo

--- src/video.py
+++ src/video.py
@@ -17,7 +17,8 @@ class Video(BaseAPI):
         self._views_count: int = int(self._json_item_['statistics']['viewCount'])
         self._likes_count: int = int(self._json_item_['statistics']['likeCount'])
 
-    def get_youtube_json(self):
+    def get_youtube_json(self) -> dict:
+        """Возвращает словарь с данными о видео."""
         response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
                                               id=self._id).execute()
         return response
@@ -37,16 +38,9 @@ class Video(BaseAPI):
         return f'{self._title}'
 
 
-class PLVideo:
-    pass
+class PLVideo(Video):
+    """Класс для представления ютуб видео и плейлиста."""
 
-
-if __name__ == '__main__':
-    # Создаем два экземпляра класса
-    video1 = Video('AWX4JnAnjBE')  # 'AWX4JnAnjBE' - это id видео из ютуб
-    # video2 = PLVideo('4fObz_qw9u4', 'PLv_zOGKKxVph_8g2Mqc3LMhj0M_BfasbC')
-    assert str(video1) == 'GIL в Python: зачем он нужен и как с этим жить'
-    # assert str(video2) == 'MoscowPython Meetup 78 - вступление'
-    # print(repr(video1))
-
-
+    def __init__(self, id_: str, playlist_id: str) -> None:
+        super().__init__(id_)
+        self._playlist_id: str = playlist_id
