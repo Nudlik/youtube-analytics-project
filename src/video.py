7b67e5c0d48d28388b0664df3c0a5c37db45b227
Nudlik <gegorp@mail.com>
2023-09-11 20:41:39 +0700

    complete hm-6

--- src/video.py
+++ src/video.py
@@ -9,13 +9,24 @@ class Video(BaseAPI):
     def __init__(self, id_: str) -> None:
         self._id: str = id_
 
-        self._json_: dict = self.get_youtube_json()
-        self._json_item_: dict = self._json_['items'][0]
+        try:
+            self._json_: dict = self.get_youtube_json()
+            self._json_item_: dict = self._json_['items'][0]
 
-        self._title: str = self._json_item_['snippet']['title']
-        self._url: str = f'https://www.youtube.com/watch?v={self._id}'
-        self._views_count: int = int(self._json_item_['statistics']['viewCount'])
-        self._likes_count: int = int(self._json_item_['statistics']['likeCount'])
+            self._title: str = self._json_item_['snippet']['title']
+            self._url: str = f'https://www.youtube.com/watch?v={self._id}'
+            self._views_count: int = int(self._json_item_['statistics']['viewCount'])
+            self._likes_count: int = int(self._json_item_['statistics']['likeCount'])
+        except IndexError:
+            self._title = self._url = self._views_count = self._likes_count = None
+
+    @property
+    def title(self) -> str:
+        return self._title
+
+    @property
+    def like_count(self) -> int:
+        return self._likes_count
 
     def get_youtube_json(self) -> dict:
         """Возвращает словарь с данными о видео."""
